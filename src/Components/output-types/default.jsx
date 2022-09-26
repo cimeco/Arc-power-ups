d229e1dc490a2228d35dd0c897e8b2089713f562
Franco2107 <fpautasso@lavozdelinterior.com.ar>
2022-09-26 16:23:55 -0300

    default raíz

--- src/Components/output-types/default.jsx
+++ src/Components/output-types/default.jsx
@@ -1,32 +1,21 @@
 import _ from "lodash";
-import PropTypes from "prop-types";
 import React from "react";
 import DefaultDataFactory from "./data-factory/default";
 
 const getTemplate = props => {
   const getFromDataFactory = () => {
     return <DefaultDataFactory {...props} />;
-
+  };
   const templates = {
     getFromDataFactory
   };
-  return templates[`getFrom${_.upperFirst(_.camelCase(props.arcSite))}`]();
+  return templates[`getFrom${_.upperFirst(_.camelCase(props))}`]();
 };
 
 const Default = props => {
   return getTemplate(props);
 };
 
-Default.propTypes = {
-  arcSite: PropTypes.string,
-  children: PropTypes.any,
-  requestUri: PropTypes.any,
-  contextPath: PropTypes.string,
-  deployment: PropTypes.func,
-  globalContent: PropTypes.any,
-  globalContentConfig: PropTypes.any,
-  Fusion: PropTypes.any,
-  Libs: PropTypes.any
-};
+
 
 export default Consumer(Default);
