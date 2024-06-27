e4453804e2917de9e95345d3e580c4f8b9f3f301
andreshaitit <andreshaitit99@gmail.com>
2024-06-27 13:07:25 -0300

    form recipe

--- src/Components/powerups/recipe/Edit.jsx
+++ src/Components/powerups/recipe/Edit.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { parseQueryString, sendMessage } from "../../../../util/powerups";
-import { customDecodeURIComponent } from "../../../../util/helpers";
 
 export const loader = async () => {
   sendMessage("ready", {
@@ -14,34 +13,59 @@ export const loader = async () => {
 
   return { recipe };
 };
+
 const RecipeEdit = () => {
   const { recipe } = useLoaderData();
+
   const [duration, setDuration] = useState(recipe.duration);
   const [calories, setCalories] = useState(recipe.calories);
   const [difficulty, setDifficulty] = useState(recipe.difficulty);
+  const [cookTime, setCookTime] = useState(recipe.cookTime);
+  const [prepTime, setPrepTime] = useState(recipe.prepTime);
+  const [keywords, setKeywords] = useState(recipe.keywords);
+  const [nutritionCalories, setNutritionCalories] = useState(
+    recipe.nutrition.calories
+  );
+  const [recipeYield, setRecipeYield] = useState(recipe.recipeYield);
+  const [recipeCategory, setRecipeCategory] = useState(recipe.recipeCategory);
   const [ingredients, setIngredients] = useState(recipe.ingredients);
   const [newIngredient, setNewIngredient] = useState("");
   const [editIndex, setEditIndex] = useState(null);
+  const [recipeInstructions, setRecipeInstructions] = useState(
+    recipe.recipeInstructions
+  );
+  const [newStep, setNewStep] = useState("");
 
   const saveData = (e) => {
     e.preventDefault();
 
-    const recipe = {
+    const updatedRecipe = {
       duration,
       calories,
       difficulty,
+      cookTime,
+      prepTime,
+      keywords,
+      nutrition: {
+        calories: nutritionCalories,
+      },
+      recipeYield,
+      recipeCategory,
       ingredients,
+      recipeInstructions,
     };
 
     const ansCustomEmbed = {
       id: parseQueryString()["k"],
       url: "https://dsj9tz56eff78.cloudfront.net/powerups/recipe/view",
       config: {
-        recipe,
+        recipe: updatedRecipe,
       },
     };
+
     sendMessage("data", ansCustomEmbed);
   };
+
   const addIngredient = (e) => {
     e.preventDefault();
     if (editIndex !== null) {
@@ -68,76 +92,238 @@ const RecipeEdit = () => {
     setIngredients(ingredients.filter((_, i) => i !== index));
   };
 
+  const addStep = (e) => {
+    e.preventDefault();
+    setRecipeInstructions([
+      ...recipeInstructions,
+      { name: `Paso ${recipeInstructions.length + 1}`, text: newStep },
+    ]);
+    setNewStep("");
+  };
+
+  const editStep = (event, index) => {
+    event.preventDefault();
+    setNewStep(recipeInstructions[index].text);
+    setRecipeInstructions(recipeInstructions.filter((_, i) => i !== index));
+  };
+
+  const removeStep = (event, index) => {
+    event.preventDefault();
+    setRecipeInstructions(recipeInstructions.filter((_, i) => i !== index));
+  };
+
   useEffect(() => {
     sendMessage("ready", { height: document.documentElement.scrollHeight });
   }, []);
 
+  const timeOptions = [
+    "5 min",
+    "10 min",
+    "15 min",
+    "20 min",
+    "30 min",
+    "45 min",
+    "1 hr",
+    "1.5 hr",
+    "2 hr",
+    "2.5 hr",
+    "3 hr",
+    "4 hr",
+  ];
+
   return (
-    <div className="w-full max-w-lg">
-      <form className="rounded px-8 pt-6 pb-8 mb-4" onSubmit={saveData}>
-        <div className="p-4 flex flex-col space-y-4">
-          <div className="bg-white shadow-md rounded-lg p-4">
-            <div className="flex items-center space-x-2">
-              <span className="font-semibold">Duración:</span>
-              <input
-                className="border rounded-md px-2"
-                onChange={(e) => setDuration(e.target.value)}
-                value={duration}
-              />
-            </div>
-            <div className="flex items-center space-x-2 mt-2">
-              <span className="font-semibold">Calorías:</span>
-              <input
-                className="border rounded-md px-2"
-                onChange={(e) => setCalories(e.target.value)}
-                value={calories}
-              />
-            </div>
-            <div className="flex items-center space-x-2 mt-2">
-              <span className="font-semibold">Dificultad:</span>
-              <input
-                className="border rounded-md px-2"
-                onChange={(e) => setDifficulty(e.target.value)}
-                value={difficulty}
-              />
-            </div>
+    <div className="w-full">
+      <form className="rounded p-4 bg-white flex" onSubmit={saveData}>
+        <div className="w-1/2 p-2">
+          <div className="flex flex-col space-y-2">
+            <span className="font-semibold">Duración (tiempo total):</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setDuration(e.target.value)}
+              value={duration}
+            />
+            <span className="text-gray-600 text-sm">
+              Tiempo total que toma preparar la receta (incluyendo preparación y
+              cocción).
+            </span>
           </div>
-          <div className="bg-white shadow-md rounded-lg p-4">
-            <h2 className="font-bold">Ingredientes</h2>
-            <ul className="list-disc list-inside mt-2">
-              {ingredients.map((ingredient, index) => (
-                <li key={index}>
-                  {ingredient.item}
-                  <button
-                    onClick={(e) => editIngredient(e, index)}
-                    className="text-blue-400 ml-2"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    onClick={(e) => removeIngredient(e, index)}
-                    className="text-red-600 ml-1"
-                  >
-                    Eliminar
-                  </button>
-                </li>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Calorías:</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setCalories(e.target.value)}
+              value={calories}
+            />
+            <span className="text-gray-600 text-sm">
+              Número de calorías por porción.
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Dificultad:</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setDifficulty(e.target.value)}
+              value={difficulty}
+            />
+            <span className="text-gray-600 text-sm">
+              Nivel de dificultad para preparar la receta (fácil, media,
+              difícil).
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Tiempo de cocción:</span>
+            <select
+              className="border rounded-md px-2"
+              onChange={(e) => setCookTime(e.target.value)}
+              value={cookTime}
+            >
+              {timeOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
               ))}
-            </ul>
-            <div>
-              <input
-                className="border rounded-md w-72 px-2"
-                value={newIngredient}
-                onChange={(e) => setNewIngredient(e.target.value)}
-              />
-              <button
-                className="border rounded-md px-2 ml-2 mt-2"
-                onClick={addIngredient}
-              >
-                {editIndex !== null ? "Actualizar" : "Agregar"}
-              </button>
-            </div>
+            </select>
+            <span className="text-gray-600 text-sm">
+              Seleccione el tiempo de cocción (horas y minutos).
+            </span>
           </div>
-          <div className="flex justify-center">
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Tiempo de preparación:</span>
+            <select
+              className="border rounded-md px-2"
+              onChange={(e) => setPrepTime(e.target.value)}
+              value={prepTime}
+            >
+              {timeOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <span className="text-gray-600 text-sm">
+              Seleccione el tiempo de preparación (horas y minutos).
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">
+              Palabras clave (separadas por comas):
+            </span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setKeywords(e.target.value)}
+              value={keywords}
+            />
+            <span className="text-gray-600 text-sm">
+              Ingrese otros términos descriptivos de la receta, como la estación
+              ("verano"), el día festivo ("Halloween") y otras palabras que la
+              describen ("rápida", "fácil", "original"). Separe cada palabra
+              clave con una coma. No use términos que sean categorías o tipos de
+              cocina.
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Calorías por porción:</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setNutritionCalories(e.target.value)}
+              value={nutritionCalories}
+            />
+            <span className="text-gray-600 text-sm">
+              Cantidad de calorías en cada porción de la receta.
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Porciones que rinde:</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setRecipeYield(e.target.value)}
+              value={recipeYield}
+            />
+            <span className="text-gray-600 text-sm">
+              Número de porciones que produce la receta.
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2 mt-2">
+            <span className="font-semibold">Categoría de la receta:</span>
+            <input
+              className="border rounded-md px-2"
+              onChange={(e) => setRecipeCategory(e.target.value)}
+              value={recipeCategory}
+            />
+            <span className="text-gray-600 text-sm">
+              Tipo de comida o plato de la receta (cena, plato principal,
+              postre, bocadillo, etc.).
+            </span>
+          </div>
+        </div>
+        <div className="w-1/2 p-2">
+          <h2 className="font-bold">Ingredientes</h2>
+          <ul className="list-disc list-inside mt-2">
+            {ingredients.map((ingredient, index) => (
+              <li key={index}>
+                {ingredient.item}
+                <button
+                  onClick={(e) => editIngredient(e, index)}
+                  className="text-blue-400 ml-2"
+                >
+                  Editar
+                </button>
+                <button
+                  onClick={(e) => removeIngredient(e, index)}
+                  className="text-red-600 ml-1"
+                >
+                  Eliminar
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <input
+              className="border rounded-md w-4/5 px-2"
+              value={newIngredient}
+              onChange={(e) => setNewIngredient(e.target.value)}
+            />
+            <button
+              className="border rounded-md px-2 ml-2 mt-2"
+              onClick={addIngredient}
+            >
+              {editIndex !== null ? "Actualizar" : "Agregar"}
+            </button>
+          </div>
+          <h2 className="font-bold mt-5">Pasos de la receta</h2>
+          <ol className="list-decimal list-inside mt-2">
+            {recipeInstructions.map((step, index) => (
+              <li key={index}>
+                {step.text}
+                <button
+                  onClick={(e) => editStep(e, index)}
+                  className="text-blue-400 ml-2"
+                >
+                  Editar
+                </button>
+                <button
+                  onClick={(e) => removeStep(e, index)}
+                  className="text-red-600 ml-1"
+                >
+                  Eliminar
+                </button>
+              </li>
+            ))}
+          </ol>
+          <div>
+            <input
+              className="border rounded-md w-4/5 px-2"
+              value={newStep}
+              onChange={(e) => setNewStep(e.target.value)}
+            />
+            <button
+              className="border rounded-md px-2 ml-2 mt-2"
+              onClick={addStep}
+            >
+              {editIndex !== null ? "Actualizar" : "Agregar"}
+            </button>
+          </div>
+          <div className="flex justify-end mt-10">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
--- src/Components/powerups/recipe/View.jsx
+++ src/Components/powerups/recipe/View.jsx
@@ -3,20 +3,20 @@ import { sendMessage, parseQueryString } from "../../../../util/powerups";
 import { customDecodeURIComponent } from "../../../../util/helpers";
 
 export const loader = async () => {
-
   sendMessage("ready", {
     height: document.documentElement.scrollHeight,
   });
 
   const parameters = Object.assign({ wait: 0 }, parseQueryString());
-  const data = JSON.parse( decodeURIComponent(parameters.p) );
-  const recipe = data.config.recipe
+  const data = JSON.parse(decodeURIComponent(parameters.p));
+  const recipe = data.config.recipe;
 
   return { recipe };
 };
 
 const RecipeView = () => {
-  const {recipe} = useLoaderData();
+  const { recipe } = useLoaderData();
+  console.log(recipe, "recipe");
 
   return (
     <div className="p-4 flex flex-col space-y-4">
@@ -33,18 +33,51 @@ const RecipeView = () => {
           <span className="font-semibold">Dificultad:</span>
           <span>{recipe.difficulty}</span>
         </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Tiempo de cocción:</span>
+          <span>{recipe.cookTime}</span>
+        </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Tiempo de preparación:</span>
+          <span>{recipe.prepTime}</span>
+        </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Palabras clave:</span>
+          <span>{recipe.keywords}</span>
+        </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Calorías por porción:</span>
+          <span>{recipe.nutrition.calories}</span>
+        </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Porciones que rinde:</span>
+          <span>{recipe.recipeYield}</span>
+        </div>
+        <div className="flex items-center space-x-2 mt-2">
+          <span className="font-semibold">Categoría de la receta:</span>
+          <span>{recipe.recipeCategory}</span>
+        </div>
       </div>
       <div className="bg-white shadow-md rounded-lg p-4">
         <h2 className="font-bold">Ingredientes</h2>
         <ul className="list-disc list-inside mt-2">
-          {recipe.ingredients.map((item, index) =>
-            (
-              <li key={index}>{item.item}</li>
-            )
-          )}
+          {recipe.ingredients.map((item, index) => (
+            <li key={index}>{item.item}</li>
+          ))}
         </ul>
+      </div>
+      <div className="bg-white shadow-md rounded-lg p-4">
+        <h2 className="font-bold">Pasos de la receta</h2>
+        <ol className="list-decimal list-inside mt-2">
+          {recipe.recipeInstructions.map((step, index) => (
+            <li key={index}>
+              <span className="font-semibold">{step.name}:</span> {step.text}
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
 };
+
 export default RecipeView;
